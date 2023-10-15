712c322359c8ef3aa78a318c1b1ad98e512e93d9
Joel Storr <joel.storr@ewe.net>
2023-10-15 22:39:10 +0200

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Joel Storr on 15.10.23.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
